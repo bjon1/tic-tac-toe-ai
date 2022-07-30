ba8a2dc90b9a94120be8aa6a85e3a8003ffc0d09
bjon1 <bcjon3@gmail.com>
2022-07-30 02:38:18 -0400

    Add function template for JS

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 window.addEventListener('DOMContentLoaded', () => {
     
+    /*
+        Some Variables:
+        .tile, .player-turn, #reset, .display-announce
+    
+    */
+
+    function takeAction(){
+
+    }
+    
+    function isValidMove(){
+
+    }
+
+    function updateBoard(){
+
+    }
+
+    function changePlayer(){
+
+    }
+
+    function checkWin(){
+
+    }
+
+    function announceWin(){
+
+    }
+
+    function resetBoard(){
+
+    }
+
+    /* 
+        if tile is clicked, call takeAction()
+        use addEventListener on the reset button
+    */
+
+
+
+
 
 
 
