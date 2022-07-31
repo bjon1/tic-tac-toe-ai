71d855f022282862ec0463ef2ea9c4ed28a8ee0c
bjon1 <bcjon3@gmail.com>
2022-07-31 19:11:57 -0400

    Write Various Functions

--- index.js
+++ index.js
@@ -1,63 +1,76 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
-    /*
-        Some Variables:
-        .tile, .player-turn, #reset, .display-announce
-    
-    */
 
-    function takeAction(){
+    const tiles = Array.from(document.querySelectorAll('.tile')); //Array
+    const turn = document.querySelector('.player-turn');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.display-announce'); //will be updated when winner is declared
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    let board = ['','','','','','','','','',''];
+
+    tiles.forEach((tile, index) => {
+        tile.addEventListener('click', () => takeAction(tile, index));
+    });
+
+    resetButton.addEventListener('click', () => resetBoard());
+
+    function takeAction(tile, index) {
+        if(isValidMove(tile)){
+            updateBoard(tile, index);
+            changePlayer();
+        }
     }
     
-    function isValidMove(){
-
+    function isValidMove(tile) {
+        if(tile.innerHTML == 'X' || tile.innerHTML == 'O'){
+            return false;
+        }
+        return true;
     }
 
-    function updateBoard(){
-
+    function updateBoard(tile, index) {
+        board[index] = turn.innerHTML;
+        tile.innerHTML = board[index];
+        tile.classList.add(`player-style`); //adds a class to the tile so the css can take effect
+        checkWin();
     }
 
-    function changePlayer(){
-
+    function changePlayer() {
+        if(turn.innerHTML == 'X'){
+            turn.innerHTML = 'O';
+        } else {
+            turn.innerHTML = 'X';
+        }
     }
 
-    function checkWin(){
-
+    function checkWin() {
+        /*
+            use winningConditions to check the win
+        */
     }
 
-    function announceWin(){
-
+    function announceWin() {
+        /* Implement announcerWin by displaying who has won and graying out the game */
     }
 
-    function resetBoard(){
-
+    function resetBoard() {
+        console.log("reset");
+        board = ['','','','','','','','','',''];
+        turn.innerHTML = 'X';
+        /* must reset ANNOUNCER */
+        tiles.forEach(tile => {
+            tile.innerHTML = '';            
+        });
+        
     }
 
-    /* 
-        if tile is clicked, call takeAction()
-        use addEventListener on the reset button
-    */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+});
